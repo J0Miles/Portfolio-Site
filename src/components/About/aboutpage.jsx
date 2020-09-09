945dad4d20ae5504fd2d3bedb631b9d798bf1c8c
J0Miles <jordanamiles@hotmail.com>
2020-09-09 16:04:16 -0500

    Finished about page content and added styling to remove large logo on mobile

--- src/components/About/aboutpage.jsx
+++ src/components/About/aboutpage.jsx
@@ -6,8 +6,29 @@ function About(props) {
   return (
     <div>
       <Hero title={props.title} />
-      <Content>Hello</Content>
-      {/* TODO: Write out body paragraph */}
+      <Content>
+        <p>
+          Hello my name is Jordan Miles. I'm a full stack engineer with
+          experience with React, Angular, Express JS, Node.js, SQL, MySQL,
+          MongoDB, and Python.
+        </p>
+        <p>My dream is to do freelance development as an entrepreneur.</p>
+        <p>
+          I am always trying to learn new things, currently those things include
+          deep learning utilizing Python, WordPress, Azure, and Vue js.
+        </p>
+        <p>
+          I have worked as both a full stack and front end developer in diverse
+          work enviroments. I enjoy building responsive UI's and redefining the
+          User Experience. When I'm not working chances are I am working on a
+          side project. You can find those on my github profile from the
+          homepage or&nbsp;
+          <a href="https://github.com/J0Miles" rel="noopener noreferrer">
+            here
+          </a>
+          .
+        </p>
+      </Content>
     </div>
   );
 }
